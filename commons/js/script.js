a17fdc47e785b92719f42a892638afab22a4262b
Sergey Shelkov <shelkov.ser@gmail.com>
2018-04-02 22:21:30 +0600

    Fixed slider and add slider-box

--- commons/js/script.js
+++ commons/js/script.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-    nav: false,
-	dots: true,
-    items: 1
-  })
+    /* Slider height */
+    var headerHeight = $("header").height(),
+        windowHeight = $(window).height();
+
+    $(".item img").css('height', windowHeight - headerHeight + 'px');
+
+    /* Slider */
+    $('.owl-carousel').owlCarousel({
+        nav: false,
+        dots: true,
+        items: 1
+    });
+
+    /* Box in slider */
+    $(".show-box-btn").click(function(){
+        $(".box-info").slideToggle(500);
+		$(".show-box-btn").toggleClass("hide-box-btn");
+    });
 });
+
